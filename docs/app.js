03ed29c1ee2b59e9bfeecc2297dd67ae7c29b7a9
Michael Beale <michael.beale@autodesk.com>
2018-09-18 11:37:19 -0700

    added return to normal mode

--- docs/app.js
+++ docs/app.js
@@ -40,16 +40,26 @@ function initializeViewer() {
 
     function onPixelCompareExtensionLoaded(pixelCompareExt) {
 		var offsetMode = false;
+/*
+        const LeafletDiffModes = {
+            NORMAL_DIFF: 0,
+            MODEL_A_ONLY: 1,
+            MODEL_B_ONLY: 2,
+            SPLIT_VIEW: 3,
+            HIGHLIGHT_A: 4,
+            HIGHLIGHT_B: 5
+        };
+*/
 
         function onKeyDown(event) {
         	if (!event.keyCode) return;
-        	if ((event.keyCode < 49) || (event.keyCode > 54)) return;
+        	if ((event.keyCode < 49) || (event.keyCode > 55)) return;
 
             if (event.keyCode == 49) {
                 offsetMode = !offsetMode;
                 pixelCompareExt.setChangeOffsetMode( offsetMode );
             } else
-	            pixelCompareExt.setDiffMode(event.keyCode - 49);
+	            pixelCompareExt.setDiffMode(event.keyCode - 50);
         };
         window.addEventListener('keydown', onKeyDown);
 
